6df53cdb6aab51b5b62cd9633e4f6b0e0f9ee308
gregcargo <greg@react-digital.com>
2018-07-12 20:36:02 +0400

    assignment 8 completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -55,6 +55,14 @@ class Album extends Component {
       	this.play();
     }
 
+	handleNextClick() {
+    	const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      	const newIndex = Math.max(0, currentIndex + 1);
+      	const newSong = this.state.album.songs[newIndex];
+      	this.setSong(newSong);
+      	this.play(); 
+    }   
+
 	handleHoverOn(song) {
 		console.log("hover test");
 	}
@@ -116,6 +124,7 @@ class Album extends Component {
            			currentSong={this.state.currentSong}
            			handleSongClick={() => this.handleSongClick(this.state.currentSong)}
            			handlePrevClick={() => this.handlePrevClick()}
+           			handleNextClick={() => this.handleNextClick()}
          		/>
 			</section>
 		);
